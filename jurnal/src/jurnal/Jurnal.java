1713e30d47eb325b540fbaae9adcd4603d1e3932
Aghia Firrizqika <ghiagiul@gmail.com>
2017-04-08 11:39:39 +0700

    Menjumlahkan deret Fibonacci

--- jurnal/src/jurnal/Jurnal.java
+++ jurnal/src/jurnal/Jurnal.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class Jurnal {
     
     public static void main(String[] args) {
-        
+         
         Scanner input = new Scanner(System.in);
         System.out.print("Masukkan Deret Fibonacci : ");
         int n = input.nextInt();
         long fib[] = new long[n];
+        long hasil = 0;
          
         fib[0] = 1;
         fib[1] = 1;
@@ -20,7 +21,10 @@ public class Jurnal {
          
         for (int i = 0; i < n; i++) {
             System.out.print(fib[i] +  " ");
+            hasil = fib[i] + hasil;
         }
+        System.out.println("");
+        System.out.println("Hasil : " + hasil);
     
     }
     
