850b59f102a0a9f294dbaa05c2a33189ba70a536
Aghia Firrizqika <ghiagiul@gmail.com>
2017-04-08 11:31:29 +0700

    Deret Fibonacci
    Deret Fibonacci

---
+++ jurnal/src/jurnal/Jurnal.java
@@ -0,0 +1,27 @@
+package jurnal;
+
+import java.util.Scanner;
+
+public class Jurnal {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan Deret Fibonacci : ");
+        int n = input.nextInt();
+        long fib[] = new long[n];
+         
+        fib[0] = 1;
+        fib[1] = 1;
+         
+        for(int i = 2; i < n; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+         
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib[i] +  " ");
+        }
+    
+    }
+    
+}
